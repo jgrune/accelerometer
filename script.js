ab118cfafd643ca9a244cc793e133cbe33967108
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-12-01 20:59:48 -0500

    adds ball

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ if (window.DeviceMotionEvent==undefined) {
 
 window.addEventListener("deviceorientation", handleOrientation, true)
 
+var absolute;
+var alpha;
+var beta;
+var gamma;
+
 function handleOrientation(event){
   var absolute = event.absolute;
   var alpha = event.alpha;
   var beta = event.beta;
   var gamma = event.gamma;
 
-   document.getElementById('absolute').innerHTML = absolute
+  document.getElementById('absolute').innerHTML = absolute
   document.getElementById('alpha').innerHTML = alpha
   document.getElementById('beta').innerHTML = beta
   document.getElementById('gamma').innerHTML = gamma
+
+  moveBall(beta, gamma);
+}
+
+function moveBall(x, y){
+  var ball = getElementById('ball')
+  ball.style.left = x;
+  ball.style.top = y;
 }
